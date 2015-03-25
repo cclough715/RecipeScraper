7abd2c39de8e32fd9fa54d0392089cbf4b0e43e4
cclough715 <chadnathanclough@gmail.com>
2015-03-25 16:21:17 -0400

    lists are now serialized to a file

--- cookstrScraper.py
+++ cookstrScraper.py
@@ -5,15 +5,16 @@
 import urllib3
 import bs4
 from bs4 import BeautifulSoup
-import sys
 import time
+import pickle
+
 
 http = urllib3.PoolManager()
 
 class Recipe:
     def __init__(self, name, author):
-        self.name = name
-        self.author = author
+        self.name = name.encode('ascii', 'ignore')
+        self.author = author.encode('ascii', 'ignore')
         self.attributes = []
         self.ingredients = []
 
@@ -27,10 +28,10 @@ class Recipe:
         return obj
 		
     def add_ingredient(self, ingredient):
-        self.ingredients.append(ingredient)
+        self.ingredients.append(ingredient.encode('ascii', 'ignore'))
 
     def add_attribute(self, attribute):
-        self.attributes.append(attribute)
+        self.attributes.append(attribute.encode('ascii', 'ignore'))
 
 def get_cookstr_data(url):
     try:
@@ -72,6 +73,10 @@ def get_recipe(url):
 
     return dish
 	
+def save_object(obj, path):
+    with open(path, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+    
 def get_recipes(n):
     """
         Gets n recipes from cookstr
@@ -85,23 +90,27 @@ def get_recipes(n):
     recipe_list = list()
     
     #retrieve the recipes
-    try:
-        for i in range(1, n):
+    for i in range(1, n):
+        try:
             dish = get_recipe(url)
-            
+            #check for duplicate recipes
             if dish in recipe_list:
                 i = i - 1
                 continue
             else:
                 recipe_list.append(dish)
-        time.sleep(0.25) #prevent ddos
-    except Exception, e:
-        print("Error = " + str(e))
+                time.sleep(0.25) #prevent ddos
+        except Exception, e:
+            print("Error = " + str(e))
 
     return recipe_list
 		
 if __name__ == '__main__':
-    recipes = get_recipes(10)
-    for r in recipes:
+    recipes = get_recipes(1000)
+    save_object(recipes, 'recipes.p')
+    
+    file = open("recipes.p", 'rb')
+    object_file = pickle.load(file)
+    for r in object_file:
         print r
         print '\n'
